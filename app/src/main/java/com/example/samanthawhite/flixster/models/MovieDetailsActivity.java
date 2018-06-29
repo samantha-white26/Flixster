08416d585cbd75eaf9a9b8eb6226bf7108420add
Samantha White <samanthawhite@fb.com>
2018-06-29 13:36:27 -0700

    Extra Activity Added - new UI - partial butter knife

--- app/src/main/java/com/example/samanthawhite/flixster/models/MovieDetailsActivity.java
+++ app/src/main/java/com/example/samanthawhite/flixster/models/MovieDetailsActivity.java
@@ -10,21 +10,30 @@ import com.example.samanthawhite.flixster.R;
 
 import org.parceler.Parcels;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MovieDetailsActivity extends AppCompatActivity {
 
+    @BindView(R.id.tvTitle) TextView tvTitle;
+    @BindView(R.id.tvOverview) TextView tvOverview;
+    @BindView(R.id.rbVoteAverage) RatingBar rbVoteAverage;
+
     Movie movie;
-    TextView tvTitle;
-    TextView tvOverview;
-    RatingBar rbVoteAverage;
+//    TextView tvTitle;
+//    TextView tvOverview;
+//    RatingBar rbVoteAverage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
 
-        tvTitle = (TextView) findViewById(R.id.tvTitle);
-        tvOverview = (TextView) findViewById(R.id.tvOverview);
-        rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
+        ButterKnife.bind(this);
+
+//        tvTitle = (TextView) findViewById(R.id.tvTitle);
+//        tvOverview = (TextView) findViewById(R.id.tvOverview);
+//        rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
 
         //unwrap the movie passed from the intent using its simple key name as the key and then use this specific movie
         movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
